12caa8639c8662cd7452b345fa3221da65792593
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-04-13 08:28:12 -0400

    update/replace/create
    updated file, replaced it with new code, and created additional code to further complete the assignment. 
    
    Still missing session

--- Dojo_Survey/server.py
+++ Dojo_Survey/server.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from xml.etree.ElementTree import Comment
+from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
+app.secret_key = 'Can-I-Has-Survey-Meow_Prease'
+
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
+@app.route('/results', methods = ['POST'])
+def user_results():
+    print("Survey Received")
+    name = request.form['name'],
+    location = request.form['location'],
+    language = request.form['language'],
+    comment = request.form['comment']
+    print(name,location,language,comment)
+    return render_template("results.html", name=request.form['name'], location=request.form['location'], language=request.form['language'], comment=request.form['comment'])
+# redirect isn't functioning on button click
+    return redirect("/")
 
-
+# not sure how to have results page return back to home page
+# @app.route("/reset")
+# def reset_session():
+#     session.clear()
+#     return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
